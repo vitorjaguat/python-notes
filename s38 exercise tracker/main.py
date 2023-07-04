e6a472cb672eb8a7f25edcb2bb69c585cd7feb31
Vitor Butkus <vitorbutkus@gmail.com>
2023-07-04 19:45:18 -0300

    finish exercise tracker with Google sheets project

--- s38 exercise tracker/main.py
+++ s38 exercise tracker/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime as dt
 
 # Transform user input in organized data using the natural language api from NutriotionIx
 
@@ -25,9 +26,26 @@ nutr_body = {
 response = requests.post(url=nutr_endpoint, headers=nutr_headers, json=nutr_body)
 
 exercise_data = response.json()
+print(exercise_data)
 
 
 # Inserting organized data in Google Sheets via SheetsAPI
 
-sheet_endpoint = 'https://api.sheety.co/8f1bbcc5c1484f27433771ec3c8c8d59/myWorkouts/workouts'
+sheety_endpoint = 'https://api.sheety.co/8f1bbcc5c1484f27433771ec3c8c8d59/myWorkouts/workouts'
+sheety_headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + os.environ.get('SHEETY_TOKEN')
+}
+sheety_body = {
+    "workout": {
+        "date": dt.date.today().strftime('%d/%m/%Y'),
+        "time": dt.datetime.now().strftime('%H:%M:%S'),
+        "exercise": exercise_data['exercises'][0]['name'].title(),
+        "duration": exercise_data['exercises'][0]['duration_min'],
+        "calories": exercise_data['exercises'][0]['nf_calories']
+    }
+}
 
+response = requests.post(url=sheety_endpoint, headers=sheety_headers, json=sheety_body)
+# response.raise_for_status()run 34min
+print(response.text)
