0b263dddf4ad66ef7390dae18d1bd62c04760d65
Vitor Butkus <vitorbutkus@gmail.com>
2023-07-15 21:11:02 -0300

    working on SQL database of movies

---
+++ s64 best movies/main.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///best-movies.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+    
+
+with app.app_context():
+    db.create_all()
+
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+# with app.app_context():
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
+#     db.session.add(second_movie)
+#     db.session.commit()
+
+all_movies = []
+
+
+@app.route("/")
+def home():
+    with app.app_context():
+        all_movies = db.session.execute(db.select(Movie).order_by(Movie.ranking)).scalars().all()
+    return render_template("index.html", movies=all_movies)
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        movie_id = request.form['id']
+        movie_to_edit = db.get_or_404(Movie, movie_id)
+        movie_to_edit.review = request.form['review']
+        movie_to_edit.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    movie_id = request.args.get('id')
+    movie_selected = db.get_or_404(Movie, movie_id)
+    return render_template('edit.html', movie=movie_selected)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
